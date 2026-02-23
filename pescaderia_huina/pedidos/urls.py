f171ca3711b462d43b3de820c8eec4aac9fee3a6
Cristian Esneider Rodriguez Bohorquez <sneiderrodriguez0618gmail.com>
2026-02-23 11:26:56 -0500

    Nueva actualizacion del modulo Pedidos

--- pescaderia_huina/pedidos/urls.py
+++ pescaderia_huina/pedidos/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('editar/<int:id>/', views.editar_pedido, name='editar_pedido'),
     path('eliminar/<int:id>/', views.eliminar_pedido, name='eliminar_pedido'),
     path('detalle/<int:id>/', views.detalle_pedido, name='detalle_pedido'),
+    path('cambiar-estado/<int:pedido_id>/', views.cambiar_estado_pedido, name='cambiar_estado_pedido'),
     
 ]
